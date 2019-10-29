bd222ee9817f4837f7962182981fc1b4ece95bc3
Shain-Carrasco <shaincarrasco@gmail.com>
2019-10-28 22:41:04 -0700

    completed 2/3 reducers

--- src/reducers/currentVideo.js
+++ src/reducers/currentVideo.js
@@ -1,7 +1,16 @@
 import Redux from 'redux';
+import exampleVideoData as sampleData from '../data/exampleVideoData.js';
 
-var currentVideoReducer = (state, action) => {
+var currentVideoReducer = (state = sampleData, action) => {
   //TODO: define a reducer for the currentVideo field of our state.
+  if (action.type === 'CHANGE_VIDEO') {
+    const newState = {};
+    newState.currentVideo = action.video;
+    newState.videoList = [...state.videoList];
+    return newState;
+  } else {
+    return state;
+  }
 };
 
 export default currentVideoReducer;
--- src/reducers/videoList.js
+++ src/reducers/videoList.js
@@ -1,7 +1,16 @@
 import Redux from 'redux';
+import exampleVideoData as sampleData from '../data/exampleVideoData.js';
 
 var videoListReducer = (state = sampleData, action) => {
   //TODO: define a reducer for the videoList field of our state.
+  if ( action.type === 'CHANGE_VIDEO_LIST' ) {
+    const newState = {};
+    newState.videoList = action.videos;
+    newState.currentVideo = state.currentVideo;
+    return newState;
+  } else {
+    return state;
+  }
 };
 
 export default videoListReducer;
