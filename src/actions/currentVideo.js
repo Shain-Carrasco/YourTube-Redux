c5310316be0c420c7b8241586c89ce7120a26a84
Shain-Carrasco <shaincarrasco@gmail.com>
2019-10-29 21:04:41 -0700

    creating a searchHandler

--- src/actions/currentVideo.js
+++ src/actions/currentVideo.js
@@ -1,8 +1,8 @@
 var changeVideo = (video) => {
   //TODO:  Return some action object to change the currently playing video.
   let action = {
-    'type': 'CHANGE_VIDEO',
-    'video': video
+    type: 'CHANGE_VIDEO',
+    video: video
   };
   return action;
 };
--- src/actions/search.js
+++ src/actions/search.js
@@ -5,12 +5,35 @@ import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
 var handleVideoSearch = (q) => {
-  return {
-    type: 'HANDLE_VIDEO_SEARCH',
-    payload: q
-  };
+  // return {
+  //   type: 'HANDLE_VIDEO_SEARCH',
+  //   payload: q
+  // };
+
+  searchYouTube({ key: YOUTUBE_API_KEY, query: q }, callback);
+
+  return () => {
+    dispatch( changeVideo(  ) )
+    dispatch( changeVideoList(  ) )
+  }
 };
 export default handleVideoSearch;
 // in the state of the store we'll keep track of
 // type: 'SEARCH'
 // payload: q  (the actual value of the search input /event.target.value)
+
+/*What should handle VideoSearch do?
+
+* make a $get request by invoking searchYouTube({q,max:5, API_Key}, and a callback)
+
+return a function - Thunk?
+    - which invokes dispatch( changeVideo(data.items[0])  )
+    - which invokes dispatch( changeVideoList(data.items) )
+    -
+
+
+
+
+
+
+*/
